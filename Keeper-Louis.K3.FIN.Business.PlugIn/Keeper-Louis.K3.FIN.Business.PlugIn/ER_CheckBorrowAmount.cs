5796f94a866433fde58ef77c6624c8a686dd37c4
keeper-louis <keeper_louis@sina.com>
2018-04-26 15:49:53 +0800

    员工报销时，显示借款余额 by keeper 2018/4/26 15:49

--- Keeper-Louis.K3.FIN.Business.PlugIn/Keeper-Louis.K3.FIN.Business.PlugIn/ER_CheckBorrowAmount.cs
+++ Keeper-Louis.K3.FIN.Business.PlugIn/Keeper-Louis.K3.FIN.Business.PlugIn/ER_CheckBorrowAmount.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Keeper_Louis.K3.FIN.Business.PlugIn
 {
-    [Description("费用报销模块检查申请人借款余额")]
+    [Description("费用报销模块检查申请人借款余额,数据来源科目余额表和总账凭证")]
     public class ER_CheckBorrowAmount:AbstractBillPlugIn
     {
         public override void DataChanged(Kingdee.BOS.Core.DynamicForm.PlugIn.Args.DataChangedEventArgs e)
@@ -23,7 +23,26 @@ namespace Keeper_Louis.K3.FIN.Business.PlugIn
                 long proPoserId = Convert.ToInt64(e.NewValue);
                 if (proPoserId>0)
                 {
-                    Decimal borrowAmount = this.ShowCheckedBorrowAmount(proPoserId);
+                    String voucherIds = "";//需要处理的凭证ID
+                    //查询法人账簿中自定义字段年份期间FKLYEARPERIOD为null的凭证id,根据FYEAR,FPERIOD组合，为FKLYEARPERIOD赋值
+                    string strSql = string.Format(@"/*dialect*/SELECT  FVOUCHERID FROM T_GL_VOUCHER WHERE FACCOUNTBOOKID = 963793 and FINVALID = 0 and FDOCUMENTSTATUS = 'C' AND (FKLYEARPERIOD = NULL OR FKLYEARPERIOD = ' ' OR FKLYEARPERIOD = '')");
+                    DynamicObjectCollection WaitDealVoucher = DBUtils.ExecuteDynamicObject(this.Context,strSql);
+                    if (WaitDealVoucher!=null&& WaitDealVoucher.Count()>0)
+                    {
+                        foreach (DynamicObject item in WaitDealVoucher)
+                        {
+                            voucherIds += ",'" + item["FVOUCHERID"].ToString() + "'";
+                        }
+                        voucherIds = voucherIds.Substring(1);
+                        List<string> updateArray = new List<string>();
+                        string updateSql_1 = string.Format(@"/*dialect*/UPDATE T_GL_VOUCHER SET FKLYEARPERIOD = CONCAT(FYEAR,FPERIOD) WHERE FPERIOD>=10 AND FVOUCHERID IN ({0})", voucherIds);
+                        updateArray.Add(updateSql_1);
+                        string updateSql_2 = string.Format(@"/*dialect*/UPDATE T_GL_VOUCHER SET FKLYEARPERIOD = CONCAT(FYEAR,0,FPERIOD) WHERE FPERIOD<10 AND FVOUCHERID IN ({0})", voucherIds);
+                        updateArray.Add(updateSql_2);
+                        DBUtils.ExecuteBatch(this.Context, updateArray, 100);
+                    }
+                    Decimal borrowAmount = this.ShowCheckedBorrowAmountFormLedger(proPoserId);
+                    //Decimal borrowAmount = this.ShowCheckedBorrowAmount(proPoserId);
                     this.Model.SetValue("FCheckBorrowAmt", borrowAmount > 0 ? borrowAmount : 0);
                 }
                 else
@@ -35,6 +54,85 @@ namespace Keeper_Louis.K3.FIN.Business.PlugIn
             {
                 this.Model.SetValue("FCheckBorrowAmt", 0);
             }
+        }
+
+        /// <summary>
+        /// 计算申请人借款余额，数据来源：总账凭证、科目余额
+        /// </summary>
+        /// <param name="proPoserId"></param>
+        /// <returns></returns>
+        private decimal ShowCheckedBorrowAmountFormLedger(long proPoserId)
+        {
+            Decimal amountSum = 0;
+            //取科目余额表中倒数第二期的合计数
+            string amountFirSql = string.Format(@"SELECT sum(FENDBALANCE) ENDBALANCE,FYEARPERIOD
+FROM T_GL_Balance
+WHERE FACCOUNTID IN (
+		SELECT FACCTID
+		FROM T_BD_ACCOUNT
+		WHERE fnumber LIKE '1221.%'
+			OR FNUMBER = '1123.05'
+	)
+	AND FACCOUNTBOOKID = 963793
+	AND FCURRENCYID = 1
+	AND FDETAILID IN (
+		SELECT fid
+		FROM T_BD_FLEXITEMDETAILV
+		WHERE FFLEX7 = '{0}'
+	)
+	AND FYEARPERIOD = (
+		SELECT TOP 1 *
+		FROM (
+			SELECT DISTINCT TOP 2 FYEARPERIOD
+			FROM T_GL_Balance
+			WHERE FACCOUNTBOOKID = 963793
+			ORDER BY FYEARPERIOD DESC
+		) mid
+		ORDER BY mid.FYEARPERIOD ASC
+	)
+	GROUP BY FYEARPERIOD", proPoserId);
+            DynamicObjectCollection amountFirCollection = DBUtils.ExecuteDynamicObject(this.Context,amountFirSql);
+            if (amountFirCollection!=null&&amountFirCollection.Count()>0)
+            {
+                Decimal amountFir = Convert.ToDecimal(amountFirCollection[0]["ENDBALANCE"]);
+                string yearPeriod = amountFirCollection[0]["FYEARPERIOD"].ToString();
+                string sysYear = DateTime.Now.Year.ToString();
+                string sysMonth = DateTime.Now.Month.ToString();
+                string sysYearPeriod = Convert.ToInt32(sysMonth) < 10 ? sysYear + "0" + sysMonth : sysYear + sysMonth;
+                //获取总账凭证中借款数据
+                string AmountSecSql = string.Format(@"SELECT SUM(vry.FDEBIT) - SUM(vry.FCREDIT) as AmountSec
+FROM T_GL_VOUCHER vr
+	INNER JOIN T_GL_VOUCHERENTRY vry ON vr.FVOUCHERID = vry.FVOUCHERID
+WHERE vry.FACCOUNTID IN (
+		SELECT FACCTID
+		FROM T_BD_ACCOUNT
+		WHERE fnumber LIKE '1221.%'
+			OR FNUMBER = '1123.05'
+	)
+	AND vr.FKLYEARPERIOD>'{0}'
+	AND vr.FKLYEARPERIOD<='{1}'
+	AND vr.FACCOUNTBOOKID = 963793
+	AND vry.FDETAILID IN (
+		SELECT fid
+		FROM T_BD_FLEXITEMDETAILV
+		WHERE FFLEX7 = '{2}'
+	)
+	AND FINVALID = 0
+	AND FDOCUMENTSTATUS = 'C'",yearPeriod, sysYearPeriod, proPoserId);
+                DynamicObjectCollection amountSecCollection = DBUtils.ExecuteDynamicObject(this.Context, AmountSecSql);
+                if (amountSecCollection!=null&&amountSecCollection.Count()>0)
+                {
+                    amountSum = amountFir + Convert.ToDecimal(amountSecCollection[0]["AmountSec"]);
+                }
+            }
+            if (amountSum>0)
+            {
+                return amountSum;
+            }
+            else
+            {
+                return 0;
+            } 
         }
 
         /// <summary>
