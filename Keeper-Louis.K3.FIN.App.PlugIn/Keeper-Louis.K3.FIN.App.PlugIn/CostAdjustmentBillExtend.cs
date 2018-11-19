5be21b3ce1bee619f5175e283301172be363d86a
Keeper_louis <keeper_louis@sina.com>
2018-11-19 22:15:41 +0800

    采取执行计划的方式30min刷新一次出库成本核算单 by KEEPER at 2018/11/19

--- Keeper-Louis.K3.FIN.App.PlugIn/Keeper-Louis.K3.FIN.App.PlugIn/CostAdjustmentBillExtend.cs
+++ Keeper-Louis.K3.FIN.App.PlugIn/Keeper-Louis.K3.FIN.App.PlugIn/CostAdjustmentBillExtend.cs
@@ -9,6 +9,7 @@ using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
 using Kingdee.BOS.Orm.DataEntity;
 using Kingdee.BOS.App.Data;
 using System.Data;
+using Kingdee.BOS.Log;
 
 namespace Keeper_Louis.K3.FIN.App.PlugIn
 {
@@ -28,7 +29,7 @@ namespace Keeper_Louis.K3.FIN.App.PlugIn
                 {
                     long biiID = Convert.ToInt64(item["Id"]);
                     DynamicObjectCollection entityCollection = item["HS_AdjustmentBillEntry"] as DynamicObjectCollection;
-                    if (Convert.ToString(item["BillTYpe"]).Equals("4d20243e32b14834aae8a96525354bcd"))//入库成本调整单
+                    if (Convert.ToString(item["BillTYpe_Id"]).Equals("4d20243e32b14834aae8a96525354bcd"))//入库成本调整单
                     {
                         foreach (DynamicObject entity in entityCollection)
                         {
@@ -56,20 +57,21 @@ namespace Keeper_Louis.K3.FIN.App.PlugIn
                             }
                         }
                     }
-                    if (Convert.ToString(item["BillTYpe"]).Equals("d040f688e69b406186aecfb5d9a86101"))//出库成本调整单
-                    {
-                        foreach (DynamicObject entity in entityCollection)
-                        {
-                            string billNo = Convert.ToString(entity["SrcBillNo"]);
-                            string strSql = string.Format(@"/*dialect*/select distinct a.FProjectBillNo from T_HS_AdjustmentBillEntry a inner join T_HS_AdjustmentBill b on a.FID = b.FID where b.FBILLNO = '{0}'", billNo);
-                            long xmxxID = DBUtils.ExecuteScalar<long>(this.Context, strSql, -1, null);
-                            if (xmxxID != -1)
-                            {
-                                string updateSql = string.Format(@"update T_HS_AdjustmentBillEntry set FProjectBillNo = {0} where FSRCBILLNO = '{1}' and fid = {2}", xmxxID, billNo, biiID);
-                                DBUtils.Execute(this.Context, updateSql);
-                            }
-                        }
-                    }
+                    //if (Convert.ToString(item["BillTYpe_Id"]).Equals("d040f688e69b406186aecfb5d9a86101"))//出库成本调整单
+                    //{
+                    //    foreach (DynamicObject entity in entityCollection)
+                    //    {
+                    //        string billNo = Convert.ToString(entity["SrcBillNo"]);
+                            
+                    //        string strSql = string.Format(@"/*dialect*/select distinct a.FProjectBillNo from T_HS_AdjustmentBillEntry a inner join T_HS_AdjustmentBill b on a.FID = b.FID where b.FBILLNO = '{0}'", billNo);
+                    //        long xmxxID = DBUtils.ExecuteScalar<long>(this.Context, strSql, -1, null);
+                    //        if (xmxxID != -1)
+                    //        {
+                    //            string updateSql = string.Format(@"update T_HS_AdjustmentBillEntry set FProjectBillNo = {0} where FSRCBILLNO = '{1}' and fid = {2}", xmxxID, billNo, biiID);
+                    //            DBUtils.Execute(this.Context, updateSql);
+                    //        }
+                    //    }
+                    //}
                     
                 }
             }
---
+++ Keeper-Louis.K3.FIN.App.PlugIn/Keeper-Louis.K3.FIN.App.PlugIn/CostUpdateScheduleService.cs
@@ -0,0 +1,40 @@
+﻿using Kingdee.BOS.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kingdee.BOS;
+using Kingdee.BOS.Core;
+using Kingdee.BOS.App.Data;
+using System.Data;
+using System.ComponentModel;
+
+namespace Keeper_Louis.K3.FIN.App.PlugIn
+{
+    [Description("1小时对出库成本核算单进行一次抓取更新项目号")]
+    public class CostUpdateScheduleService : IScheduleService
+    {
+        public void Run(Context ctx, Schedule schedule)
+        {
+            //项目号为null，单据类型为出库成本核算单，查询出来distinct源单据号、FID
+            string strSql = string.Format(@"/*dialect*/select distinct X.FSRCBILLNO,Z.FID FROM T_HS_AdjustmentBill Z INNER JOIN T_HS_AdjustmentBillEntry X ON Z.FID = X.FID AND Z.FBILLTYPEID = 'd040f688e69b406186aecfb5d9a86101'  AND X.FProjectBillNo = ' ' AND X.FSRCBILLNO <> ' ' AND X.FPROJECTBILLNO <> 0");
+            //循环结果集，根据来源单据号查询项目号
+            using (IDataReader reader = DBUtils.ExecuteReader(ctx,strSql))
+            {
+                while (reader.Read())
+                {
+                    string proSql = string.Format(@"/*dialect*/select distinct a.FProjectBillNo from T_HS_AdjustmentBillEntry a inner join T_HS_AdjustmentBill b on a.FID = b.FID where b.FBILLNO = '{0}'", Convert.ToString(reader["FSRCBILLNO"]));
+                    long xmxxID = DBUtils.ExecuteScalar<long>(ctx, proSql, -1, null);
+                    //根据来源单据号和FID,对核算单赋值
+                    if (xmxxID != -1)
+                    {
+                        string updateSql = string.Format(@"update T_HS_AdjustmentBillEntry set FProjectBillNo = {0} where FSRCBILLNO = '{1}' and fid = {2}", xmxxID, Convert.ToString(reader["FSRCBILLNO"]), Convert.ToInt64(reader["FID"]));
+                        DBUtils.Execute(ctx, updateSql);
+                    }
+                }
+            }
+            
+        }
+    }
+}
